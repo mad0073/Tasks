31304f66cca2032350029922fcd9f51d8236c453
mad0073 <mad0073@mix.wvu.edu>
2022-01-13 17:21:50 -0500

    MykalDaniel Task 01 upload b

--- Tasks/Task_01/task01.r
+++ Tasks/Task_01/task01.r
@@ -1,3 +1,20 @@
 # password for git via terminal: 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
 install.packages("swirl")
 library(swirl)
+MykalDaniel
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+my_sqrt
+my_div <- z / my_sqrt
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
