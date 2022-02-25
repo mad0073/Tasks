72abc5c9f02f6ba7639e0f93d03d325e06e3cf01
mad0073 <mad0073@mix.wvu.edu>
2022-02-25 09:19:31 -0500

    MykalDaniel Task 07

---
+++ Tasks/Task_07/task07.r
@@ -0,0 +1,6 @@
+install.packages("rvertnet")
+setwd('var/folders/pf/lpnm75v13bs_jlj1qpqw26w80000gn/T//RtmpdTrwZu/downloaded_packages')
+"rvertnet-packages"
+vertsearch(taxon = "aves", state = "california")
+read.csv('RA_EHA_DATA.csv')
+Hypothesis: There is a positive correlation for aggressive behavior and unfavorable environmental effects.
